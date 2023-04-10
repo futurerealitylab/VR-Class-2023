af559290e3970081e787a2d0addc66b138a0e97c
Matt-Kou <36983956+Matt-Kou@users.noreply.github.com>
2023-04-10 15:29:50 -0400

    Added Basketball player

--- js/scenes/bbCoaching.js
+++ js/scenes/bbCoaching.js
@@ -4,22 +4,26 @@ import {Gltf2Node} from "../render/nodes/gltf2.js";
 
 class Player {
     constructor(gltfUrl) {
-        this.gltfNode = new Gltf2Node({url: gltfUrl})
-        global.gltfRoot.addNode(this.gltfNode);
+        this.node = new Gltf2Node({url: gltfUrl})
+        global.gltfRoot.addNode(this.node);
         this.position = null
         this.direction = 0
     }
 
     update() {
-        this.gltfNode.matrix = cg.mMultiply(cg.mTranslate(this.position), cg.mRotateY(this.direction))
+        if (this.position) {
+            this.node.matrix = cg.mMultiply(cg.mTranslate(this.position), cg.mRotateY(this.direction))
+        }
     }
 }
 
 export const init = async model => {
+    model.setTable(false)
+    model.setRoom(true)
     let playerList = []
     const numPlayers = 5
     for (let i = 0; i < numPlayers; i++) {
-        playerList.push(new Player("gltf path"))
+        playerList.push(new Player("./media/gltf/Basketball_Player/Basketball_Player.gltf"))
     }
     model.animate(() => {
         for (let i = 0; i < numPlayers; i++) {

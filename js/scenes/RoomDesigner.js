4c27a3c4d3cbc720f8f98c979752477c18f3c38e
iamomar <email@email.com>
2023-04-17 19:08:22 -0400

    Updated Controller Movement

--- js/scenes/RoomDesigner.js
+++ js/scenes/RoomDesigner.js
@@ -1,6 +1,17 @@
 import { g2 } from "../util/g2.js";
 import { controllerMatrix, buttonState, joyStickState } from "../render/core/controllerInput.js";
+import * as cg from "../render/core/cg.js";
 
+
+let leftTriggerPrev = false;
+let rightTriggerPrev = false;
+let boxScale = 0.1;
+const colorChangeSpeed = 0.01;
+let boxColor = { r: 1, g: 1, b: 1 };
+
+let MP = cg.mTranslate(0,1.5,.5);
+let A = [0,0,0];
+let MA = cg.mIdentity();
 export const init = async model => {
 
    model.setTable(false);
@@ -18,7 +29,7 @@ export const init = async model => {
 
    // const floor = model.add('cube').move(0, -1, 0).scale(100, 0.01, 100)
 
-   let whiteBoard = model.add('cube').texture(() => {
+   let whiteBoard = model.add('cube').move(0,2.2,0).scale(.5,.5,.5).texture(() => {
       g2.setColor('white');
       g2.fillRect(.1,0,.8,1);
       g2.setColor('black');
@@ -115,6 +126,27 @@ export const init = async model => {
    const drawnWalls = [];
 
    const floor = model.add("cube").move(0, 0.5, 0).scale(roomScale, 0.001, roomScale)
+
+
+
+   // CREATE THE BOX.
+
+   let box = model.add('cube');
+
+   // FUNCTION TO RETURN TRUE IF A POINT IS INSIDE THE BOX, OTHERWISE FALSE.
+
+   let isInBox = p => {
+
+      // FIRST TRANSFORM THE POINT BY THE INVERSE OF THE BOX'S MATRIX.
+
+      let q = cg.mTransform(cg.mInverse(box.getMatrix()), p);
+
+      // THEN WE JUST NEED TO SEE IF THE RESULT IS INSIDE A UNIT CUBE.
+
+      return q[0] >= -1 & q[0] <= 1 &&
+               q[1] >= -1 & q[1] <= 1 &&
+               q[2] >= -1 & q[2] <= 1 ;
+   }
       
    model.animate(() => {
 
@@ -125,7 +157,7 @@ export const init = async model => {
       }
       leftButtonPrev = leftButtonCurr
 
-      whiteBoard.hud().scale(1, 1, .0001).opacity(showWhiteboard ? 1 : 0);
+      whiteBoard.scale(1, 1, .0001).opacity(showWhiteboard ? 1 : 0);
 
       const walls = whiteBoard.getWalls(roomScale)
 
@@ -169,5 +201,117 @@ export const init = async model => {
       else if(!buttonState.left[1].pressed){
          Condswitch = false;
       }
+
+
+      // FETCH THE MATRIXES FOR THE LEFT AND RIGHT CONTROLLER.
+
+      //let ml = controllerMatrix.left;
+      let mr = controllerMatrix.right;
+
+      // EXTRACT THE LOCATION OF EACH CONTROLLER FROM ITS MATRIX,
+      // AND USE IT TO SEE WHETHER THAT CONTROLLER IS INSIDE THE BOX.
+
+      let isLeftInBox  = isInBox(ml.slice(12,15));
+      let isRightInBox = isInBox(mr.slice(12,15));
+
+      // IF NEITHER CONTROLLER IS INSIDE THE BOX, COLOR THE BOX WHITE.
+
+      if (! isLeftInBox && ! isRightInBox)
+         box.color(1,1,1);
+
+      // IF THE LEFT CONTROLLER IS INSIDE THE BOX
+
+      if (isLeftInBox) {
+
+         // COLOR THE BOX PINK.
+
+         box.color(1,.5,.5);
+
+         // IF THE LEFT TRIGGER IS SQUEEZED
+
+         let leftTrigger = buttonState.left[0].pressed;
+	 if (leftTrigger) {
+
+            // COLOR THE BOX RED AND MOVE THE BOX.
+
+            box.color(1,0,0);
+            let B = ml.slice(12,15);
+            if (! leftTriggerPrev)         // ON LEFT DOWN EVENT:
+               A = B;                      // INITIALIZE PREVIOUS LOCATION.
+            else
+               MP = cg.mMultiply(cg.mTranslate(cg.subtract(B, A)), MP);
+
+	    A = B;                         // REMEMBER PREVIOUS LOCATION.
+         }
+         leftTriggerPrev = leftTrigger;
+      }
+
+      // IF THE RIGHT CONTROLLER IS INSIDE THE BOX
+
+      if (isRightInBox) {
+
+         // COLOR THE BOX LIGHT BLUE.
+
+         box.color(.5,.5,1);
+
+	 // IF THE RIGHT TRIGGGER IS SQUEEZED
+
+         let rightTrigger = buttonState.right[0].pressed;
+	 if (rightTrigger) {
+
+	    // COLOR THE BOX BLUE AND MOVE AND ROTATE THE BOX.
+
+            box.color(0,0,1);
+            let MB = mr.slice();
+            if (! rightTriggerPrev)        // ON RIGHT DOWN EVENT:
+               MA = MB;                    // INITIALIZE PREVIOUS MATRIX.
+            else
+	       MP = cg.mMultiply(cg.mMultiply(MB, cg.mInverse(MA)), MP);
+
+	    MA = MB;                       // REMEMBER PREVIOUS MATRIX.
+         }
+         rightTriggerPrev = rightTrigger;
+      }
+      if (isRightInBox) {
+           // SCALE THE BOX USING RIGHT JOYSTICK CONTROLLER
+
+           let rightJoyX = joyStickState.right.x;
+           if (rightJoyX > 0.5) {
+               boxScale += 0.01;
+           } else if (rightJoyX < -0.5) {
+               boxScale -= 0.01;
+               if (boxScale < 0.01) {
+                   boxScale = 0.01;
+               }
+           }
+   }
+   /*  
+   if (isRightInBox) {
+
+      // COLOR THE BOX BASED ON CURRENT COLOR VALUES
+      //ADJUST COLOR OF THE BOX USING RIGHT JOYSTICK CONTROLLER
+   let rightJoyX = joyStickState.right.x;
+   if (rightJoyX > 0.5) {
+      boxColor.g = Math.min(boxColor.g + colorChangeSpeed, 1); 
+      boxColor.b = Math.max(boxColor.b - colorChangeSpeed, 0); 
+   } else if (rightJoyX < -0.5) {
+      boxColor.g = Math.max(boxColor.g - colorChangeSpeed, 0); 
+      boxColor.b = Math.min(boxColor.b + colorChangeSpeed, 1); 
+   }
+
+}
+
+*/
+
+       // Get joystick movement
+       const joystickX = joyStickState.left.x;
+       const joystickY = joyStickState.left.y;
+       const joystickZ = joyStickState.right.y;
+       const speed = 0.01;
+       const moveVector = [joystickX * speed, - joystickZ * speed, joystickY * speed];
+       MP = cg.mMultiply(cg.mTranslate(moveVector), MP);
+      // DISPLAY THE BOX.
+      box.color(boxColor.r, boxColor.g, boxColor.b);
+      box.setMatrix(MP).scale(boxScale).scale(1, wallHeight, wallThickness+ 0.4).color('red');
    });
 }

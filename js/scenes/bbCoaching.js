c8631413de336a36b67d08c4ec04ccb8f09b9c30
Matt-Kou <36983956+Matt-Kou@users.noreply.github.com>
2023-05-01 14:16:03 -0400

    Added linear interpolation for player movements

--- js/scenes/bbCoaching.js
+++ js/scenes/bbCoaching.js
@@ -3,71 +3,66 @@ import * as cg from "../render/core/cg.js";
 import {Gltf2Node} from "../render/nodes/gltf2.js";
 import {g2} from "../util/g2.js";
 import {buttonState, joyStickState} from "../render/core/controllerInput.js";
+import {COLORS, MAX_TIME} from "./const.js";
+import {resampleCurve} from "../render/core/cg.js";
 
-let colors = ['#32a852', '#3e32a8', '#eaf04d', '#e36e14', '#f50fb7'];
 
-let hudIsShown = true;                                              // press right[1] to show hud, press again to hide it
+let hudIsShown = true;      // press right[1] to show hud, press again to hide it
 let hudButtonLock = false;
 let hudButtonHandler = () => {
-    if (buttonState.right[1].pressed && !hudButtonLock) {
+    if (buttonState.right[1] && buttonState.right[1].pressed && !hudButtonLock) {
         hudIsShown = !hudIsShown;
         hudButtonLock = true;
     }
 
-    if (!buttonState.right[1].pressed) {
+    if (buttonState.right[1] && !buttonState.right[1].pressed) {
         hudButtonLock = false;
     }
 }
-
 
 class Player {
     constructor(gltfUrl, index, initialPosition, c) {
         this.node = new Gltf2Node({url: gltfUrl});
         global.gltfRoot.addNode(this.node);
-        this.direction = 0;
-        // this.position = initialPosition;
         this.initialPosition = initialPosition;
         this.index = index;
         this.color = c;
-        // store the position of each time point for this player
-        this.positions = [];
-        // check if a specific time point is within a movement, and be used to find the start/end of a move.
-        this.isMoving = [];
-        for (let i = 0; i < 24; i++) {
-            this.positions.push([initialPosition[0], initialPosition[1], initialPosition[2]]);
-            this.isMoving.push(false);
-        }
+        // store the position of each time frame for this player
+        this.positions = Array.from({length: 24}, () => Object.assign([], initialPosition));
+        this.directions = Array(24).fill(0);
+        // check if a specific time frame is within a movement, and be used to find the start/end of a move.
+        this.isMoving = Array(24).fill(false);
     }
 
-    // get the start and end timepoint of the move
+    // get the start and end time frame of the move
     // if no action selected return [24,23]
     // if start point selected but no end point return [s,s], s is the selected point.
     // if start and end both selected, return [s,e], s < e.
     getStartAndEnd() {
-        let point = 0;
-        while (point < this.isMoving.length && this.isMoving[point] != true) {
-            point++;
+        let t = 0;
+        while (t < MAX_TIME && !this.isMoving[t]) {
+            t++;
         }
-        let start = point;
-        while (point < this.isMoving.length && this.isMoving[point] != false) {
-            point++;
+        let start = t;
+        while (t < MAX_TIME && this.isMoving[t]) {
+            t++;
         }
-        let end = point - 1;
+        let end = t - 1;
         return [start, end];
     }
-    
-    pos3D() {
-        return this.position
+
+    pos3D(t) {
+        return this.positions[t]
     }
 
-    pos2D() {
-        return this.position.slice(0, 2)
+    pos2D(t) {
+        return this.positions[t].slice(0, 2)
     }
 
-    update() {
-        if (this.position) {
-            this.node.matrix = cg.mMultiply(cg.mTranslate(this.position[0] * Court.width, this.position[2], -this.position[1] * Court.height), cg.mRotateY(this.direction))
-        }
+    update(t) {
+        // console.log("directions:", this.directions)
+        // console.log("positions:", this.positions)
+        this.node.matrix = cg.mMultiply(cg.mTranslate(this.positions[t][0] * Court.width, this.positions[t][2], -this.positions[t][1] * Court.height), cg.mRotateY(this.directions[t]))
     }
 }
 
@@ -79,7 +74,6 @@ class Court {
         this.node = new Gltf2Node({url: gltfUrl})
         global.gltfRoot.addNode(this.node)
         this.direction = 0
-
     }
 
     static position2DTo3D(pos2D) {
@@ -87,19 +81,15 @@ class Court {
     }
 }
 
-let markTime = () =>{
-
-}
-
-
 export const init = async model => {
     model.setTable(false)
     model.setRoom(false)
+    let currTime = 0
     let currCourt = new Court('./media/gltf/bbCourt/scene.gltf')
     let playerList = []
     const numPlayers = 5
     for (let i = 0; i < numPlayers; i++) {
-        playerList.push(new Player("./media/gltf/Basketball_Player/Basketball_Player.gltf", i, [1., (i - 2) * .2, 0], colors[i]));
+        playerList.push(new Player("./media/gltf/Basketball_Player/Basketball_Player.gltf", i, [1., (i - 2) * .2, 0], COLORS[i]));
     }
 
     let boardBase = model.add()
@@ -114,23 +104,22 @@ export const init = async model => {
         g2.textHeight(.03);
         for (let i = 0; i < playerList.length; i++) {
             let player = playerList[i];
-            let start = player.getStartAndEnd()[0];
-            let end = player.getStartAndEnd()[1];
+            const [start, end] = player.getStartAndEnd();
 
             // mark the selected player
-            if (i == tacticBoard.currPlayer) {
+            if (i === tacticBoard.currPlayer) {
                 g2.textHeight(.08);
                 g2.fillText('*', .55, .12 + i * .14, 'center');
                 g2.textHeight(.03);
             }
-    
-            if (start != 24) {
+
+            if (start !== 24) {
                 // start point selected
-                g2.fillText(start + ': ' + '(' + (player.positions[start][0].toFixed(1)) + ',' + (player.positions[start][1].toFixed(1)) + ')  ', .8, .14+i*.14, 'center');
+                g2.fillText(start + ': ' + '(' + (player.positions[start][0].toFixed(1)) + ',' + (player.positions[start][1].toFixed(1)) + ')  ', .8, .14 + i * .14, 'center');
             }
             if (start < end) {
                 // end point selected
-                g2.fillText(end + ': ' + '(' + (player.positions[end][0].toFixed(1)) + ',' + (player.positions[end][1].toFixed(1)) + ')  ', .8, .1+i*.14, 'center');
+                g2.fillText(end + ': ' + '(' + (player.positions[end][0].toFixed(1)) + ',' + (player.positions[end][1].toFixed(1)) + ')  ', .8, .1 + i * .14, 'center');
             }
         }
         g2.textHeight(.05);
@@ -142,7 +131,7 @@ export const init = async model => {
         g2.fillText('0s', .55, .78, 'center');
         g2.fillText('↑', .965, .805, 'center');
         g2.fillText('23s', .965, .78, 'center');
-        g2.fillText('-- Time Points --', .76, .79, 'center');
+        g2.fillText('-- Time Frames --', .76, .79, 'center');
         g2.drawWidgets(tacticBoard);
     });
 
@@ -150,22 +139,37 @@ export const init = async model => {
     tacticBoard.timeButton = [];                                                //array used to store the time button widgets
     tacticBoard.startTime = -1;                                                 //starting time of the player
     tacticBoard.endTime = -1;                                                   //ending time of the player
-    tacticBoard.started_setting = false;                                                //record if have started the time count or not
+    tacticBoard.started_setting = false;                                        //record if have started the time count or not
 
     g2.addTrackpad(tacticBoard, .25, .47, '#ff8080', ' ', () => {
     }, 1, playerList, tacticBoard);
     for (let i = 0; i < numPlayers; i++) {
-        g2.addWidget(tacticBoard, 'button', .65, .12 + i * .14, colors[i], '#' + i, () => {
-            tacticBoard.currPlayer = i;
-            // reset the status of tacticBoard.
-            tacticBoard.startTime = -1;
-            tacticBoard.endTime = -1;
-            tacticBoard.started_setting = false;
+        g2.addWidget(tacticBoard, 'button', .65, .12 + i * .14, COLORS[i], '#' + i, () => {
+            if (tacticBoard.currPlayer !== i) {
+                tacticBoard.currPlayer = i;
+                // reset the status of tacticBoard.
+                tacticBoard.startTime = -1;
+                tacticBoard.endTime = -1;
+                tacticBoard.started_setting = false;
+            } else {
+                let player = playerList[tacticBoard.currPlayer];
+                tacticBoard.currPlayer = -1;
+                const [start, end] = player.getStartAndEnd();
+                // const src = [player.pos3D(start), player.pos3D(end)]
+                const line = cg.resampleCurve([player.pos3D(start), player.pos3D(end)], end - start)
+                console.log("line:", line)
+                for (let i = 0; i < end - start; i++) {
+                    for (let j = 0; j < 3; j++) {
+                        player.positions[start + i][j] = line[i][j]
+                    }
+                }
+            }
         }, 0.9);
     }
 
     for (let i = 0; i < 24; i++) {
-        tacticBoard.timeButton.push(g2.addWidget(tacticBoard, 'button', .55+ i * .018, .84, '#a0aaba', " ", () => {
+        tacticBoard.timeButton.push(g2.addWidget(tacticBoard, 'button', .55 + i * .018, .84, '#a0aaba', " ", () => {
+            currTime = i;
             let player = playerList[tacticBoard.currPlayer];
             if (!tacticBoard.started_setting) {
                 for (let j = 0; j < 24; j++) {
@@ -185,7 +189,7 @@ export const init = async model => {
                         player.positions[j] = Array.from(player.initialPosition);
                     }
                 } else {
-                    for (let j = tacticBoard.startTime+1; j <= tacticBoard.endTime; j++) {
+                    for (let j = tacticBoard.startTime + 1; j <= tacticBoard.endTime; j++) {
                         player.isMoving[j] = true;
                     }
                 }
@@ -199,16 +203,16 @@ export const init = async model => {
     tacticBoard.identity().scale(.9, .9, .0001).opacity(0);
     fieldMap.identity().move(-0.45, -0.045, 0.0002).scale(.70, .76, .0001).opacity(0.2);
 
-    // update the color of each time button based on the player and time point selected
+    // update the color of each time button based on the player and time frame selected
     let updateTimeButton = () => {
-        if (tacticBoard.currPlayer == -1) {
+        if (tacticBoard.currPlayer === -1) {
             for (let j = 0; j < 24; j++) {
                 tacticBoard.timeButton[j].updateColor('#a0aaba');
             }
         } else {
             for (let j = 0; j < 24; j++) {
-                if (playerList[tacticBoard.currPlayer].isMoving[j] == true) {
-                    tacticBoard.timeButton[j].updateColor(colors[tacticBoard.currPlayer]);
+                if (playerList[tacticBoard.currPlayer].isMoving[j]) {
+                    tacticBoard.timeButton[j].updateColor(COLORS[tacticBoard.currPlayer]);
                 } else {
                     tacticBoard.timeButton[j].updateColor('#a0aaba');
                 }
@@ -218,10 +222,7 @@ export const init = async model => {
 
     model.animate(() => {
         boardBase.identity().boardHud().scale(1.3);
-        // boardBase.identity().move(0,1.5,0).scale(1.3);
-
         updateTimeButton();
-
         hudButtonHandler();
 
         if (hudIsShown) {
@@ -229,13 +230,20 @@ export const init = async model => {
                 boardBase._children.push(tacticBoard)
                 boardBase._children.push(fieldMap)
             }
-            playerList[tacticBoard.currPlayer].direction += 2 * model.deltaTime * joyStickState.right.x
+            // console.log("tacticBoard.currPlayer:", tacticBoard.currPlayer)
+            // console.log("playerList[tacticBoard.currPlayer].getStartAndEnd():", playerList[tacticBoard.currPlayer].getStartAndEnd())
+            if (tacticBoard.currPlayer !== -1) {
+                const [start, end] = playerList[tacticBoard.currPlayer].getStartAndEnd();
+                if (end > start) {
+                    playerList[tacticBoard.currPlayer].directions[end] += 2 * model.deltaTime * joyStickState.right.x
+                }
+            }
         } else {
             boardBase._children = [];
         }
 
         for (let i = 0; i < numPlayers; i++) {
-            playerList[i].update()
+            playerList[i].update(currTime)
         }
     });
 }
